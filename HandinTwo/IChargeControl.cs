583c7b42d1ce98260de486ddb9076b705ee9f4d2
AndreasErslev <89972306+AErslev@users.noreply.github.com>
2021-10-14 10:49:15 +0200

    Ny version af GitHub repo HandInTwo

---
+++ HandinTwo/IChargeControl.cs
@@ -0,0 +1,15 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandinTwo.klasser
+{
+    public interface IChargeControl
+    {
+        void DoorLock();
+        void DoorUnlock();
+        
+        public event EventHandler<CurrentEventArgs> OpenDoorEvent;
+
+    }
+}
---
+++ HandinTwo/IDisplay.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Text;
+
+namespace HandinTwo.klasser
+{
+    public interface IDisplay
+    {
+        void RFidStatus();
+        void RFidError();
+        void PhoneConnect();
+        void PhoneRemove();
+        void ConnectionError();
+        void IsOccupied(bool);
+    }
+}
